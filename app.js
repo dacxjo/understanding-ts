ecbd819def4101bb10b7c5fed5e5a383774eabfd
dacxjo <djesus1906@protonmail.com>
2021-01-03 14:47:15 +0100

    Complete basics chapter

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Whatever you want');
+function add(n1, n2, showResult, phrase) {
+    if (showResult) {
+        console.log(phrase + (n1 + n2));
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var showResult = true;
+var resultPhrase = 'The value is: ';
+add(number1, number2, showResult, resultPhrase);
